a8ad1d637710ad423950d4c468660c756118f4ee
abhinavKeshri07 <keshri.abhinav07@gmail.com>
2018-09-18 11:33:51 -0700

    added a navigation activity and logo

--- app/src/main/java/com/abhinavkeshri07/gplan/MainActivity.java
+++ app/src/main/java/com/abhinavkeshri07/gplan/MainActivity.java
@@ -1,7 +1,12 @@
 package com.abhinavkeshri07.gplan;
 
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+
+        }
+        else
+        {
+
+        }
+        Intent intent = new Intent(this, navigation.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/abhinavkeshri07/gplan/navigation.java
@@ -0,0 +1,14 @@
+package com.abhinavkeshri07.gplan;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class navigation extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation);
+    }
+
+}
